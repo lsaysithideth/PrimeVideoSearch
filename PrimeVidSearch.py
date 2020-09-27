24896df7a0b0b6ef70997aa314b116459cdba891
lsaysithideth <59379166+lsaysithideth@users.noreply.github.com>
2020-09-27 00:26:09 -0400

    Update PrimeVidSearch.py

--- PrimeVidSearch.py
+++ PrimeVidSearch.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 #create empty lists to be used in functions later
 vidLink = [] 
@@ -24,10 +26,13 @@ vidDtlSubStr = "/gp/video/detail/"
 def scrapText(lst,classType,className):
     findClass = soup.find_all(classType,class_=className)
     if len(findClass) == 0:
-        lst.append("Unknown") #helps fill in data that can't be found
+        lst.append(None) #helps fill in data that can't be found
     else:
         for n in findClass:
-            lst.append(n.text)
+            if className == ratingName: 
+                lst.append(float(n.text[-3:])) #Extract and convert rating to float
+            else:
+                lst.append(n.text)
 
 #Next initialize browser to be controlled by Python below
 #DO NOT CLOSE new browser window that is opened until end of code run.  
@@ -41,8 +46,11 @@ for elem in elems:
     if vidDtlSubStr in elem.get_attribute("href"):
         vidLink.append(elem.get_attribute("href"))
 
+#remove duplicates in vidLink list
+vidLink = list(dict.fromkeys(vidLink))
+
 #start scraping the data for each link
-for i in range(0,5): #control for performance
+for i in range(0,len(vidLink)): #control for performance 
     driver.get(vidLink[i])      
     content = driver.page_source
     soup = BeautifulSoup(content)
@@ -53,5 +61,30 @@ for i in range(0,5): #control for performance
 
 #Save the data into neat columns in a csv file
 a = {'Title':title,'Rating':rating, 'Synopsis':synopsis}
-dfA = pd.DataFrame(a)
-dfA.to_csv('PrimeVid.csv', index=False, encoding='utf-8')
+df = pd.DataFrame(a)
+df.to_csv('PrimeVid.csv', index=False, encoding='utf-8')
+
+#Creating the wordcloud by ratings groups
+# First separte dataframe into 3 different ratings groups
+dfBelow6 = df.loc[(df['Rating'] < 6)]
+df67 = df.loc[(df['Rating'] >= 6) & (df['Rating'] < 8)]
+dfAbove8 = df.loc[(df['Rating'] >= 8)]
+
+#Then write a function to create a wordcloud for each ratings group
+# Wordcloud idea from https://towardsdatascience.com/simple-wordcloud-in-python-2ae54a9f58e5
+
+def wordcloud(dataframe,filename):
+    # Read the whole text.
+    text = ' '.join(dataframe.Synopsis)
+    # Generate a word cloud image
+    wordcloud = WordCloud().generate(text)
+
+    # Display the generated image:
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+        
+    plt.savefig(filename + '.png')
+
+wordcloud(dfBelow6, "Below6")
+wordcloud(df67, "6-7")
+wordcloud(dfAbove8, "Above8")
